3b4538131da191f1444d7ffa46a826d1db12106f
isiakpereaghogho <85246849+isiakpereaghogho@users.noreply.github.com>
2026-05-30 02:29:19 +0100

    designing end_to_end ML Pipeline

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='predictive_maintenance',
+    version='1.0.0',
+    packages=find_packages(),
+    author='Aghogho Isiakpere, Olumuyiwa Olasehinde, Damilola Rebecca Abimbola'
+)
